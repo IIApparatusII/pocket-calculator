3ef7d0ecfee47a39be4b319fb26bdcc77ea81a7f
Dennis Gannon <dennisg798@gmail.com>
2019-01-07 13:22:24 -0500

    completed main css and got to display each number... that's it

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,6 +1,116 @@
 /*add changing border color*/
+function zero() {
+  var display = 0
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+  return display;
+}
+
 function one() {
-  var display = 1
+  var display = 1;
   var div=document.getElementById('calc-output');
   div.innerHTML=(`${display}`);
 }
+
+function two() {
+  var display = 2;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function three() {
+  var display = 3;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function four() {
+  var display = 4;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function five() {
+  var display = 5;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function six() {
+  var display = 6;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function seven() {
+  var display = 7;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function eight() {
+  var display = 8;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function nine() {
+  var display = 9;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function clear() {
+  var display = 0;
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function add() {
+  display = zero() + "+";
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+/*
+function two() {
+  var display = 2
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function two() {
+  var display = 2
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function two() {
+  var display = 2
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function two() {
+  var display = 2
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function two() {
+  var display = 2
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function two() {
+  var display = 2
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+
+function two() {
+  var display = 2
+  var div=document.getElementById('calc-output');
+  div.innerHTML=(`${display}`);
+}
+*/
