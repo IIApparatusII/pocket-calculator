124daf93a6e2cbe82731bdbc6635bcbd7580cfdf
Dennis Gannon <dennisg798@gmail.com>
2019-01-17 13:24:26 -0500

    basically nothing

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,5 +1,15 @@
 /*add changing border color*/
+var x = 9;
+let num1 = ""
+
 function insert(num) {
+/*  if (document.form.textview.value = 0) {
+    document.form.textview.value = " ";
+    document.form.textview.value = document.form.textview.value + num;
+  }
+  else {
+  document.form.textview.value = document.form.textview.value + num;
+}*/
   document.form.textview.value = document.form.textview.value + num;
 }
 
@@ -14,12 +24,33 @@ function equals() {
   }
 }
 
+function add() {
+  document.form.textview.value = document.form.textview.value + "+";
+}
+
+function subtract() {
+  document.form.textview.value = document.form.textview.value + "-";
+}
+
+function multiply() {
+  document.form.textview.value = document.form.textview.value + "*";
+}
+
+function divide() {
+  document.form.textview.value = document.form.textview.value + "/";
+}
+
 /*make negate work with chain of funcions --> 85+4 turns into 85+ (-4)*/
 function negate() {
   document.form.textview.value = document.form.textview.value * -1;
   if (document.form.textview.value == "") {
     document.form.textview.value = "";
   }
+/*  if (document.form.textview.value.includes("-")) {
+    document.form.textview.value = document.form.textview.value - "-";
+    document.form.textview.value = document.form.textview.value + "+";
+  }
+  */
 }
 
 function percentage() {
@@ -28,10 +59,15 @@ function percentage() {
 
 /*Figure out what replaces insert() or putting in the actual exponent*/
 function exponent() {
-  document.form.textview.value = Math.pow(document.form.textview.value, x);
+
+  document.form.textview.value = Math.pow(num1, document.form.textview.value);
 }
 
 /*Math.pow(n, 1/root); n is base value being rooted*/
 function root() {
-  
+
+}
+
+function pi() {
+  document.form.textview.value = document.form.textview.value + "&#x3C0;"
 }
