4524063a91cbf8de5f47a785dd5d2ce2e3eb1930
Dennis Gannon <dennisg798@gmail.com>
2019-01-22 13:24:47 -0500

    hella done

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,46 +1,115 @@
 /*add changing border color*/
-var x = 9;
-let num1 = ""
-
 function insert(num) {
-/*  if (document.form.textview.value = 0) {
-    document.form.textview.value = " ";
+  let num1 = document.form.textview.value;
+  let zero = "0";
+  let point = ".";
+
+
+  if (num1.length == 9) {
+    document.form.textview.value = document.form.textview.value;
+  }
+
+  else if (num1.indexOf(point) == 1) {
     document.form.textview.value = document.form.textview.value + num;
   }
+
+  else if (num1.indexOf(zero) == 0) {
+    document.form.textview.value = "" + num;
+  }
+
   else {
-  document.form.textview.value = document.form.textview.value + num;
-}*/
-  document.form.textview.value = document.form.textview.value + num;
+    document.form.textview.value = document.form.textview.value + num;
+  }
+  /*LIMITS BUT BREAKS ONCE HITTING 9 DIGITS*/
+  /*if (num1.length > 8) {
+    while (num1.length > 8) {
+      document.form.textview.value = num1.slice(num, -1);
+    }
+  }*/
 }
 
 function reset() {
-  document.form.textview.value = "";
+  document.form.textview.value = "0";
 }
 
 function equals() {
   var exp = document.form.textview.value;
-  if (exp){
-    document.form.textview.value = eval(exp);
-  }
-  if (exp == "Infinity"){
+  let num1 = document.form.textview.value;
+
+  if (document.form.textview.value == "Infinity"){
     document.form.textview.value = "Error";
+  }
+
+  else if (num1.lastIndexOf("+") == num1.length - 1 || num1.lastIndexOf("-") == num1.length - 1 || num1.lastIndexOf("*") == num1.length - 1 || num1.lastIndexOf("/") == num1.length - 1) {
+    document.form.textview.value = "Error";
+  }
+
+  else if (exp){
+    document.form.textview.value = eval(exp);
   }
 }
 
 function add() {
-  document.form.textview.value = document.form.textview.value + "+";
+  let num1 = document.form.textview.value;
+  let plus = "+";
+
+  if (num1.length == 18) {
+    document.form.textview.value = document.form.textview.value;
+  }
+
+  else if (num1.lastIndexOf(plus) == num1.length - 1) {
+    document.form.textview.value = document.form.textview.value;
+  }
+
+  else {
+    document.form.textview.value = document.form.textview.value + plus;
+  }
+}
+
+function decimal() {
+  let num1 = document.form.textview.value;
+  let point = ".";
+
+  document.form.textview.value = num1.includes(point)? document.form.textview.value : document.form.textview.value + ".";
 }
 
 function subtract() {
-  document.form.textview.value = document.form.textview.value + "-";
+  let num1 = document.form.textview.value;
+  let minus = "-";
+
+  if (num1.length == 18) {
+    document.form.textview.value = document.form.textview.value;
+  }
+  else if (num1.lastIndexOf(minus) == num1.length - 1) {
+    document.form.textview.value = document.form.textview.value;
+  }
+  else {
+    document.form.textview.value = document.form.textview.value + minus;
+  }
 }
 
 function multiply() {
-  document.form.textview.value = document.form.textview.value + "*";
+  let num1 = document.form.textview.value;
+  let times = "*";
+
+  if (num1.lastIndexOf(times) == num1.length - 1) {
+    document.form.textview.value = document.form.textview.value;
+  }
+  else {
+    document.form.textview.value = document.form.textview.value + times;
+  }
 }
 
 function divide() {
-  document.form.textview.value = document.form.textview.value + "/";
+  let num1 = document.form.textview.value;
+  let slash = "/";
+
+  if (num1.lastIndexOf(slash) == num1.length - 1) {
+    document.form.textview.value = document.form.textview.value;
+  }
+  else {
+    document.form.textview.value = document.form.textview.value + slash;
+  }
 }
 
 /*make negate work with chain of funcions --> 85+4 turns into 85+ (-4)*/
@@ -62,13 +131,14 @@ function percentage() {
 
 /*Figure out what replaces insert() or putting in the actual exponent*/
 function exponent() {
-
-  document.form.textview.value = Math.pow(num1, document.form.textview.value);
+  let num1 = document.form.textview.value;
+  document.form.textview.value = Math.pow(num1, 2);
 }
 
 /*Math.pow(n, 1/root); n is base value being rooted*/
 function root() {
-
+  let num1 = document.form.textview.value;
+  document.form.textview.value = Math.sqrt(num1);
 }
 
 function pi() {
