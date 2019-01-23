b8906fc1853731c5d8810ea2912c2a6380cb855f
IIApparatusII <43757431+IIApparatusII@users.noreply.github.com>
2019-01-22 23:44:42 -0500

    final

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,11 +1,22 @@
+//Your code goes here:
 /*add changing border color*/
+function expo(x, f) {
+  return Number.parseFloat(x).toExponential(f);
+}
+
 function insert(num) {
   let num1 = document.form.textview.value;
   let zero = "0";
   let point = ".";
-
-
-  if (num1.length == 9) {
+  let slash = "/";
+  let plus = "+";
+  let minus = "-";
+  let times = "*";
+  if (num1.length == 23) {
+    document.form.textview.value = document.form.textview.value;
+  }
+  
+  else if (num1.length == 11) {
     document.form.textview.value = document.form.textview.value;
   }
 
@@ -19,6 +30,19 @@ function insert(num) {
 
   else {
     document.form.textview.value = document.form.textview.value + num;
+    num1 = document.form.textview.value;
+      if (num1.length == 3 && num1.lastIndexOf(plus) !== 1 && num1.lastIndexOf(minus) !== 1 && num1.lastIndexOf(times) !== 1 && num1.lastIndexOf(slash) !== 1) {
+        document.form.textview.value = document.form.textview.value + ",";
+      }
+      else if (num1.length == 7 && num1.lastIndexOf(plus) !== 1 && num1.lastIndexOf(minus) !== 1 && num1.lastIndexOf(times) !== 1 && num1.lastIndexOf(slash) !== 1) {
+        document.form.textview.value = document.form.textview.value + ",";
+      }
+      else if (num1.length == 15 && num1.lastIndexOf(plus) !== 1 && num1.lastIndexOf(minus) !== 1 && num1.lastIndexOf(times) !== 1 && num1.lastIndexOf(slash) !== 1) {
+        document.form.textview.value = document.form.textview.value + ",";
+      }
+      else if (num1.length == 19 && num1.lastIndexOf(plus) !== 1 && num1.lastIndexOf(minus) !== 1 && num1.lastIndexOf(times) !== 1 && num1.lastIndexOf(slash) !== 1) {
+        document.form.textview.value = document.form.textview.value + ",";
+      }
   }
   /*LIMITS BUT BREAKS ONCE HITTING 9 DIGITS*/
   /*if (num1.length > 8) {
@@ -39,28 +63,44 @@ function equals() {
   if (document.form.textview.value == "Infinity"){
     document.form.textview.value = "Error";
   }
-
+  
   else if (num1.lastIndexOf("+") == num1.length - 1 || num1.lastIndexOf("-") == num1.length - 1 || num1.lastIndexOf("*") == num1.length - 1 || num1.lastIndexOf("/") == num1.length - 1) {
     document.form.textview.value = "Error";
   }
 
   else if (exp){
     document.form.textview.value = eval(exp);
+    num1 = document.form.textview.value
+      if (exp.length > 9) {
+        num1 = Number(num1);
+        document.form.textview.value = expo(num1, 8);
+       }
   }
+  
 }
 
 function add() {
   let num1 = document.form.textview.value;
+  let slash = "/";
   let plus = "+";
+  let minus = "-";
+  let times = "*";
 
-  if (num1.length == 18) {
+  if (num1.lastIndexOf(plus) == num1.length - 1) {
     document.form.textview.value = document.form.textview.value;
   }
-
-  else if (num1.lastIndexOf(plus) == num1.length - 1) {
-    document.form.textview.value = document.form.textview.value;
+  else if (num1.lastIndexOf(minus) == num1.length - 1) {
+    document.form.textview.value = num1.slice("-", -1)
+    document.form.textview.value = document.form.textview.value + plus;
   }
-
+  else if (num1.lastIndexOf(times) == num1.length - 1) {
+    document.form.textview.value = num1.slice("*", -1)
+    document.form.textview.value = document.form.textview.value + plus;
+  }
+  else if (num1.lastIndexOf(slash) == num1.length - 1) {
+    document.form.textview.value = num1.slice("/", -1)
+    document.form.textview.value = document.form.textview.value + plus;
+  }
   else {
     document.form.textview.value = document.form.textview.value + plus;
   }
@@ -75,13 +115,25 @@ function decimal() {
 
 function subtract() {
   let num1 = document.form.textview.value;
+  let slash = "/";
+  let add = "+";
   let minus = "-";
+  let times = "*";
 
-  if (num1.length == 18) {
+  if (num1.lastIndexOf(minus) == num1.length - 1) {
     document.form.textview.value = document.form.textview.value;
   }
-  else if (num1.lastIndexOf(minus) == num1.length - 1) {
-    document.form.textview.value = document.form.textview.value;
+  else if (num1.lastIndexOf(add) == num1.length - 1) {
+    document.form.textview.value = num1.slice("+", -1)
+    document.form.textview.value = document.form.textview.value + minus;
+  }
+  else if (num1.lastIndexOf(times) == num1.length - 1) {
+    document.form.textview.value = num1.slice("*", -1)
+    document.form.textview.value = document.form.textview.value + minus;
+  }
+  else if (num1.lastIndexOf(slash) == num1.length - 1) {
+    document.form.textview.value = num1.slice("/", -1)
+    document.form.textview.value = document.form.textview.value + minus;
   }
   else {
     document.form.textview.value = document.form.textview.value + minus;
@@ -90,10 +142,25 @@ function subtract() {
 
 function multiply() {
   let num1 = document.form.textview.value;
+  let slash = "/";
+  let add = "+";
+  let minus = "-";
   let times = "*";
-
+  
   if (num1.lastIndexOf(times) == num1.length - 1) {
     document.form.textview.value = document.form.textview.value;
+  }
+  else if (num1.lastIndexOf(add) == num1.length - 1) {
+    document.form.textview.value = num1.slice("+", -1)
+    document.form.textview.value = document.form.textview.value + times;
+  }
+  else if (num1.lastIndexOf(minus) == num1.length - 1) {
+    document.form.textview.value = num1.slice("-", -1)
+    document.form.textview.value = document.form.textview.value + times;
+  }
+  else if (num1.lastIndexOf(slash) == num1.length - 1) {
+    document.form.textview.value = num1.slice("/", -1)
+    document.form.textview.value = document.form.textview.value + times;
   }
   else {
     document.form.textview.value = document.form.textview.value + times;
@@ -103,10 +170,26 @@ function multiply() {
 function divide() {
   let num1 = document.form.textview.value;
   let slash = "/";
+  let add = "+";
+  let minus = "-";
+  let times = "*";
 
   if (num1.lastIndexOf(slash) == num1.length - 1) {
     document.form.textview.value = document.form.textview.value;
   }
+  else if (num1.lastIndexOf(add) == num1.length - 1) {
+    document.form.textview.value = num1.slice("+", -1)
+    document.form.textview.value = document.form.textview.value + slash;
+  }
+  else if (num1.lastIndexOf(minus) == num1.length - 1) {
+    document.form.textview.value = num1.slice("-", -1)
+    document.form.textview.value = document.form.textview.value + slash;
+  }
+  else if (num1.lastIndexOf(times) == num1.length - 1) {
+    document.form.textview.value = num1.slice("*", -1)
+    document.form.textview.value = document.form.textview.value + slash;
+  }
+  
   else {
     document.form.textview.value = document.form.textview.value + slash;
   }
@@ -114,9 +197,16 @@ function divide() {
 
 /*make negate work with chain of funcions --> 85+4 turns into 85+ (-4)*/
 function negate() {
-  document.form.textview.value = document.form.textview.value * -1;
-  if (document.form.textview.value == "") {
-    document.form.textview.value = "";
+  let num1 = document.form.textview.value;
+
+  if (num1 == 0) {
+    document.form.textview.value = "-";
+  }
+  else if (document.form.textview.value == "NaN") {
+    document.form.textview.value = "Error";
+  }
+  else {
+    document.form.textview.value = document.form.textview.value * -1;
   }
 /*  if (document.form.textview.value.includes("-")) {
     document.form.textview.value = document.form.textview.value - "-";
@@ -142,5 +232,10 @@ function root() {
 }
 
 function pi() {
+  if (document.form.textview.value == "0") {
+    document.form.textview.value = "3.14";
+  }
+  else {   
   document.form.textview.value = document.form.textview.value + "3.14"
+  }
 }
